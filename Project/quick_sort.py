a2c878f7aa470febeac6a99b491fa2eb75fb82f4
Nitiphum88 <nitiphum.y@kkumail.com>
2025-12-11 00:55:27 +0700

    Add quick sort implementation by Nitiphum_6033

---
+++ Project/quick_sort.py
@@ -0,0 +1,12 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+if __name__ == "__main__":
+    data = [10, 5, 2, 3, 7, 8, 1]
+    print("Sorted:", quick_sort(data))
